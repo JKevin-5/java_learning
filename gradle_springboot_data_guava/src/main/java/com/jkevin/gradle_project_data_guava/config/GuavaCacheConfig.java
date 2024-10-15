188fb16ca8b6d117f7e39e7b4ceeb6087f553e9b
Jkevin <15810880126@163.com>
2024-10-15 16:50:30 +0800

    feat: new agrale_springboot_data_caffeine

---
+++ gradle_springboot_data_guava/src/main/java/com/jkevin/gradle_project_data_guava/config/GuavaCacheConfig.java
@@ -0,0 +1,17 @@
+package com.jkevin.gradle_project_data_guava.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author mid2098
+ * @version 1.0
+ * @description: TODO
+ * @date 2024/10/15 16:24
+ */
+@EnableCaching
+@Configuration
+public class GuavaCacheConfig {
+
+
+}
